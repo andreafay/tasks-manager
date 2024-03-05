e9821bf3da082a32dbf72966ee8086038174cee7
andreafay <andrea.fay@kireygroup.com>
2024-03-05 14:25:34 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,34 @@
-import { Flex, Box, Heading, Text, Button, Spacer, HStack } from "@chakra-ui/react"
+import { UnlockIcon } from "@chakra-ui/icons"
+import { Flex, Box, Heading, Text, Button, Spacer, HStack, useToast, Avatar, AvatarBadge } from "@chakra-ui/react"
 import React from "react"
 
 export default function Navbar() {
+    const toast = useToast()
+
+    const showToast = () => {
+        toast({
+            title: "Logged out",
+            description: "Successfully logged out!",
+            duration: 5000,
+            isClosable: true,
+            status: "success",
+            position: "top",
+            icon: <UnlockIcon />
+        })
+    }
+
     return (
         <Flex as="nav" p="10px" alignItems="center" marginBottom="40px">
             <Heading as="h1">Andre Tasks</Heading>
             <Spacer />
             <HStack spacing="20px">
-                <Box bg="gray.200" p="10px">A</Box>
+                <Avatar src="/img/yoshi.png">
+                    <AvatarBadge width="1.3em" bg="teal.500">
+                        <Text fontSize="xs" color="white">3</Text>
+                    </AvatarBadge>
+                </Avatar>
                 <Text>andre@chakra-ui.com</Text>
-                <Button colorScheme="green">Logout</Button>
+                <Button colorScheme="green" onClick={showToast}>Logout</Button>
             </HStack>
         </Flex>
     )
